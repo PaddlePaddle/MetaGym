e94b5d8142f4e9bfbcea95f7849d97b37cb45c59
Bill Xue <xueeinstein@gmail.com>
2020-06-28 10:59:57 +0800

    [fix] use body velocity as target in velocity_control task

--- rlschool/quadrotor/quadrotorsim.py
+++ rlschool/quadrotor/quadrotorsim.py
@@ -313,11 +313,7 @@ class QuadrotorSim(object):
                 low=self._min_voltage, high=self._max_voltage, size=4)
             act = act.astype(np.float32)
             self.step(act, dt)
-
-            body_velocity = np.matmul(
-                self._coordination_converter_to_body,
-                self.global_velocity)
-            velocity_lst.append(list(body_velocity))
+            velocity_lst.append(list(self.global_velocity))
 
         self._restore_state(current_state)
         return velocity_lst
