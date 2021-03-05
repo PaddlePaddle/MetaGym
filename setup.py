85c715306f050bc38eaa5d267d1ec4998cb6da4b
zenghsh3 <zenghongsheng@baidu.com>
2021-03-05 15:56:35 +0800

    release v1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 import io
 from setuptools import setup, find_packages
 
-__version__ = '0.3.2'
+__version__ = '1.0'
 
 with io.open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
